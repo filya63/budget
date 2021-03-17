0b1a89a0cf4aaceef4d86a952033ff610c8b52f8
Vladislav <vladislav@Vladislav@gmail.com>
2021-03-18 01:22:52 +0400

    add fetch request user registration

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,11 +1,27 @@
 export default {
   namespaced: true,
   state: {
-    isAuth: false
+    isAuth: false,
+    userId: null,
+    idToken: null,
+    refreshToken: null,
+    expiresIn: null
   },
   mutations: {
   },
   actions: {
+    login() {
+      // действия с логином
+    },
+    async registration(_:any, payload: any) { // https://firebase.google.com/docs/reference/rest/auth
+      const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyDhoyyMUnReE0Mej8TeAV7oRUtGaREtyxE', {
+        method: 'POST',
+        body: JSON.stringify({
+          token: payload.token, // Это нужно уточнять, что за токен должен передаваться
+          returnSecureToken: true
+        })
+      })
+    }
   },
   getters: {
     getAuthStatus: (state: any):boolean => {
