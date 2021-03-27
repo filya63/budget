88746e785744405d514f50e060cf82bacbe75e51
Vladislav <vladislav@Vladislav@gmail.com>
2021-03-28 01:05:17 +0400

    install package firebase, add config

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,17 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDhoyyMUnReE0Mej8TeAV7oRUtGaREtyxE",
+    authDomain: "budget-7780c.firebaseapp.com",
+    projectId: "budget-7780c",
+    storageBucket: "budget-7780c.appspot.com",
+    messagingSenderId: "419599037126",
+    appId: "1:419599037126:web:6a5cdaaf893d3fef4eddc7",
+    measurementId: "G-DFEL9CFF19"
+};
+
+firebase.initializeApp(firebaseConfig)
 createApp(App).use(store).use(router).mount('#app')
